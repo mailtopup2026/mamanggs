25048f4dd56aa08edbdbd5b2fa46a728222b503d
mailtopup2026 <mailtopup2026@gmail.com>
2026-09-01 13:05:57 +0700

    fix: ubah format import di api create payment

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,6 +1,7 @@
-import crypto from "crypto";
+const crypto = require("crypto");
 
-export default async function handler(req, res) {
+module.exports = async function (req, res) {
+  // Pastikan hanya menerima POST request
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
@@ -85,4 +86,4 @@ export default async function handler(req, res) {
       message: error.message,
     });
   }
-}
+};
